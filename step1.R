60b32d28ad7461c0dc5c7a4889c72ad91f5df1a9
Abdulganiyu Abdulkareem <aa24adn@herts.ac.uk>
2024-11-19 19:19:09 +0000

    i have loaded the dataset in R

---
+++ step1.R
@@ -0,0 +1,9 @@
+# Load necessary libraries
+install.packages("tidyverse")
+library(tidyverse)
+
+# Load the data (replace 'file_path' with the actual path to your CSV file)
+data <- read.csv("C:/Users/hp/Desktop/A101/bank_full.csv", sep=",")
+
+
+
